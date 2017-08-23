2dfa53a9b0bcfa1b24fa9a32ecfeac43b3346423
546-37 <546-37>
2017-08-23 12:52:32 +0700

    add

--- app.js
+++ app.js
@@ -21,6 +21,60 @@ let bodyParser = require('body-parser');
 let app = express();
 app.use(bodyParser.json({type: 'application/json'}));
 
+const getCompanyIntent = 'getCompany';
+const companyArgument = 'company';
+const marketsDataKey = process.env.markets;
+const sessionIds = {};
+
+app.post('/', function (req, res) {
+  const thisSessionID = req.body.sessionId;
+  if (sessionIds[thisSessionID] === undefined ){
+      sessionIds[thisSessionID] = [thisSessionID];
+  } else {
+    sessionIds[thisSessionID].push(thisSessionID);
+  }
+  console.log (sessionIds[thisSessionID], 'Number of calls:', sessionIds[thisSessionID].length);
+
+  console.log('>>>> BODY >>>> \n\n', JSON.stringify(req.body), '\n\n');
+  console.log ('req.body=', JSON.stringify(req.body));
+  console.log ('EXTRACTED_SessionId=' , req.body.sessionId);
+  console.log ('EXTRACTED_conversation_id=' , req.body.originalRequest.data.conversation.conversation_id);
+	const assistant = new Assistant({request: req, response: res});
+  function getCompany (assistant) {
+	let company = assistant.getArgument(companyArgument);
+
+	fetch(`http://markets.ft.com/research/webservices/securities/v1/search?query=${company}&source=${marketsDataKey}`).then((data) => {
+    if (data.ok) {
+  			return data.json();
+  		}
+  	}).then((json) => {
+		fetch(`http://markets.ft.com/research/webservices/companies/v1/profile?symbols=${json.data.searchResults[0].symbol}&source=${marketsDataKey}`).then((data) => {
+      if (data.ok) {
+  				return data.json();
+  			}
+  		}).then((json) => {
+			assistant.ask(json.data.items[0].profile.description);
+		});
+  	}).catch((error) => {
+		console.log(error)
+	});
+  }
+
+  let actionMap = new Map();
+  actionMap.set(getCompanyIntent, getCompany);
+
+  assistant.handleRequest(actionMap);
+});
+
+if (module === require.main) {
+  let server = app.listen(process.env.PORT || 8080, function () {
+    let port = server.address().port;
+    console.log('App listening on port %s', port);
+  });
+}
+
+
+
 function tOut(millis){
   setTimeout(function () {
     console.log('boo')
